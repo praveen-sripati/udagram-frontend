515f42afad8fc0fc066806c14e740ee60808a3c7
Praveen Sripati <pvnsripati@gmail.com>
2021-12-30 21:47:40 +0530

    fix: Add port in env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://Udagrambackend-env-1.eba-qr2j7nwe.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://Udagrambackend-env-1.eba-qr2j7nwe.us-east-1.elasticbeanstalk.com:5432/api/v0',
 };
 
 /*
