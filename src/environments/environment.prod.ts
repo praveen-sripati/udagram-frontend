939aef56c2514f89e3630fe0fe090baeb18154d9
Praveen Sripati <pvnsripati@gmail.com>
2021-12-30 19:21:38 +0530

    fix: Resolve linting issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://Udagrambackend-env-1.eba-qr2j7nwe.us-east-1.elasticbeanstalk.com/api/v0",
+  appName: 'Udagram',
+  apiHost: 'http://Udagrambackend-env-1.eba-qr2j7nwe.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
